7e89fc37f10e7a6f59551cb7c1370f58f71463c4
Sarah <sarah.riener@gmx.net>
2017-05-03 09:29:24 +0200

    added master blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}" />
+</head>
+<body>
+
+<nav>
+    <a href="/">Home</a>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
+
+<script src="{{ URL::asset('js/all.js') }}"></script>
+
+</body>
+</html>
