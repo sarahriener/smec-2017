347894f939c63b1ac748f29b6591c7324b9b7e13
manuela_posch <manuela.posch95@gmail.com>
2017-05-04 10:54:48 +0200

    bind in require(CanvasJS) in charts.blade.php

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -1,52 +1,54 @@
 /**
- * Created by manuela_posch on 03.05.17.
- */
+* Created by manuela_posch on 03.05.17.
+*/
 @extends('layouts.app')
 
 @section('title', 'Charts')
 
 @section('charts')
+    require('CanvasJs');
 
-<div id="chartContainer" style="height: 300px; width: 100%;">
+    <div id="chartContainer" style="height: 300px; width: 100%;">
 
 
-<script type="text/javascript">
+        <script type="text/javascript">
 
-window.onload = function () {
-var chart = new CanvasJS.Chart("chartContainer",
-{
-title:{
-text: "US Mobile / Tablet OS Market Share, Dec 2012"
-},
-animationEnabled: true,
-theme: "theme2",
-data: [
-{
-type: "doughnut",
-indexLabelFontFamily: "Garamond",
-indexLabelFontSize: 20,
-startAngle:0,
-indexLabelFontColor: "dimgrey",
-indexLabelLineColor: "darkgrey",
-toolTipContent: "{y} %",
 
-dataPoints: [
-{  y: 67.34, indexLabel: "iOS {y}%" },
-{  y: 28.6, indexLabel: "Android {y}%" },
-{  y: 1.78, indexLabel: "Kindle {y}%" },
-{  y: 0.84,  indexLabel: "Symbian {y}%"},
-{  y: 0.74, indexLabel: "BlackBerry {y}%" },
-{  y: 2.06,  indexLabel: "Others {y}%"}
+            window.onload = function () {
+                var chart = new CanvasJS.Chart("chartContainer",
+                    {
+                        title: {
+                            text: "US Mobile / Tablet OS Market Share, Dec 2012"
+                        },
+                        animationEnabled: true,
+                        theme: "theme2",
+                        data: [
+                            {
+                                type: "doughnut",
+                                indexLabelFontFamily: "Garamond",
+                                indexLabelFontSize: 20,
+                                startAngle: 0,
+                                indexLabelFontColor: "dimgrey",
+                                indexLabelLineColor: "darkgrey",
+                                toolTipContent: "{y} %",
 
-]
-}
-]
-});
+                                dataPoints: [
+                                    {y: 67.34, indexLabel: "iOS {y}%"},
+                                    {y: 28.6, indexLabel: "Android {y}%"},
+                                    {y: 1.78, indexLabel: "Kindle {y}%"},
+                                    {y: 0.84, indexLabel: "Symbian {y}%"},
+                                    {y: 0.74, indexLabel: "BlackBerry {y}%"},
+                                    {y: 2.06, indexLabel: "Others {y}%"}
 
-chart.render();
-}
-</script>
-</div>
+                                ]
+                            }
+                        ]
+                    });
+
+                chart.render();
+            }
+        </script>
+    </div>
 
 
 @endsection
