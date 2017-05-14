895c3aac29b13d5b5407bf50a5952b3dad84a2e3
Theresa <theresaobermayr@gmail.com>
2017-05-14 15:23:53 +0200

    add detail.blade.php

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', 'Overview')
+
+
+@section('content')
+
+    <div class="detail">
+        <div class="detail__header">
+            Detail header
+        </div>
+
+        @include('layouts.filter')
+
+        <div class="detail__items">
+
+            @foreach ($countries as $country)
+                <div class="detail__items--item" data-country="{{ $country->code }}" data-continent="{{$country->continent_id}}">
+                    <p>{{ $country->name }}</p>
+                </div>
+            @endforeach
+        </div>
+
+    </div>
+@endsection
--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -5,18 +5,18 @@
 
 @section('content')
 
-    <div class="content">
-        <div class="content__header">
-            <div class="content__header__title">
+    <div class="overview">
+        <div class="overview__header">
+            <div class="overview__header__title">
                 <span>Global E-Commerce Facts</span>
             </div>
 
-            <div class="content__header__facts">
-                <span class="content__header__facts--fact">78%</span><span class="content__header__facts--fact">108 Mio. $$</span><span class="content__header__facts--fact">98%</span>
+            <div class="overview__header__facts">
+                <span class="overview__header__facts--fact">78%</span><span class="overview__header__facts--fact">108 Mio. $$</span><span class="overview__header__facts--fact">98%</span>
             </div>
 
-            <div class="content__header__button">
-                <span><button type="button" class="content__header__button--btn">more</button> </span>
+            <div class="overview__header__button">
+                <span><button type="button" class="overview__header__button--btn">more</button> </span>
             </div>
         </div>
 
@@ -27,42 +27,6 @@
             @foreach ($countries as $country)
                 <div class="overview__items--item" data-country="{{ $country->code }}" data-continent="{{$country->continent_id}}">
                     <p>{{ $country->name }}</p>
-                </div>
-            @endforeach
-        </div>
-
-        <div class="content__filter">
-            <div class="content__filter__search">
-                <label>
-                    <span>Choose a Region or</span>
-                    <select class="js-example-basic-single form-control content__filter__search--search">
-                        @foreach ($countries as $country)
-                            <option value="{{ $country->code }}" title="{{$country->name}}">{{ $country->name }}</option>
-                        @endforeach
-                    </select>
-                </label>
-            </div>
-
-            <div class="content__filter__tags">
-                <button type="button" class="content__filter__tags--tag">All</button>
-                @foreach ($continents as $continent)
-                    <button class="content__filter__tags--tag">{{ $continent->name }}</button>
-                @endforeach
-            </div>
-        </div>
-
-        <div class="content__items">
-
-            <!-- find functions for filter in https://laravel.com/docs/5.1/collections#method-filter -->
-            <!-- filter only works if page reloads -->
-            @foreach ($countries as $country)
-                <div class="content__items--item" data-value="{{ $country->code }}">
-                    <p>{{ $country->name }}</p>
-                    <script type="text/javascript">
-                        function myFunction() {
-                            window.open("https://www.w3schools.com");
-                        }
-                    </script>
                 </div>
             @endforeach
         </div>
