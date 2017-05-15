9bdb6f1102d32f58e0188a05fa2dba5a8d01a525
Theresa <theresaobermayr@gmail.com>
2017-05-15 10:48:59 +0200

    filter, compare, detail css

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -1,43 +1,43 @@
 @extends('layouts.app')
 
-@section('title', 'Detail')
+@section('title', 'Compare')
 
 
 @section('content')
 
-    <div class="detail">
-        <div class="detail__header">
-            Detail header
+    <div class="compare">
+        <div class="compare__header">
+            Compare header
         </div>
 
         @include('layouts.filter')
 
-        <div class="detail__items">
+        <div class="compare__items">
 
             @foreach ($countries as $country)
-                <div class="detail__items--item" data-country="{{ $country->code }}" data-continent="{{$country->continent_id}}">
+                <div class="compare__items--item" data-country="{{ $country->code }}" data-continent="{{$country->continent_id}}">
                     <p>{{ $country->name }}</p>
                 </div>
             @endforeach
         </div>
 
-        <div class="detail__compare">
-            <div class="detail__compare__select">
-                <div class="detail__compare__select--item">
+        <div class="compare__detail">
+            <div class="compare__detail__select">
+                <div class="compare__detail__select--item">
                     country 1
                 </div>
 
-                <div class="detail__compare__select--item">
+                <div class="compare__detail__select--item">
                     country 1
                 </div>
             </div>
 
-            <div class="detail__compare__data">
-                <div class="detail__compare__data--item">
+            <div class="compare__detail__data">
+                <div class="compare__detail__data--item">
                     data country 1
                 </div>
 
-                <div class="detail__compare__data--item">
+                <div class="compare__detail__data--item">
                     data country 2
                 </div>
             </div>
