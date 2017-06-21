359ce426d02e2916783c1ccf32511411cc90feab
vhaselsteiner <viktoria.haselsteiner@gmx.at>
2017-06-21 13:46:08 +0200

    Merge branch 'master' of https://github.com/sarahriener/smec-2017
    # Conflicts:
    #	resources/views/detail.blade.php

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -7,7 +7,7 @@
         @include('layouts.header')
 
         <div class="nav__top">
-            <a href="/country/{{ $country->id }}" class="nav__top__back"><span class="glyphicon glyphicon-menu-left"></span>Back to countries</a>
+            <!--a href="/country/{{ $country->id }}" class="nav__top__back"><span class="glyphicon glyphicon-menu-left"></span>Back to countries</a-->
 
             <div class="nav__top__breadcrumbs">
                 <!-- TODO breadcrumb Klasse bennenen auch in CSS -->
