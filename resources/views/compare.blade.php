082dc4a3dde2559d520990bd06867d2f3693dcf1
vhaselsteiner <viktoria.haselsteiner@gmx.at>
2017-06-21 13:47:53 +0200

    Merge branch 'master' of https://github.com/sarahriener/smec-2017
    # Conflicts:
    #	resources/views/detail.blade.php

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -15,7 +15,7 @@
                     <li><a href="/">Google Shopping Compendium</a></li> /
                     <li><a href="/overview">Overview</a></li> /
                     <li class="active"><a href="/country/{{ $country->id }}">{{ $country->name }}</a></li> /
-                    <li class="active"><a href="/compare/{{ $country->id }}">Compare with {{ $country->name }}</a></li>
+                    <li class="active">Compare with {{ $country->name }}</li>
                 </ol>
             </div>
         </div>
