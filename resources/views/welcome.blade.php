f18d5ea58dbc988dab76dcbf5b88a9608460803b
Sarah <sarah.riener@gmx.net>
2017-06-26 09:45:33 +0200

    styling of 3 facts in welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,20 +17,23 @@
         <div class="entry__text">
             <span class="entry__text__header">E-Commerce Facts</span>
             <p>
-                With the following data, we want to answer the first important questions about e-commerce. We collected important facts such as the number of inhabitants,
-                the turnover, the web usage according to the device, but also the advertising expenditures online, spatially distributed by country and prepared them visually appealing.
-                The focus is on the use of the initial information for people who need start-up assistance in e-commerce.
+                With the following data, we want to answer the first important questions about e-commerce. We collected
+                important facts such as the number of inhabitants,
+                the turnover, the web usage according to the device, but also the advertising expenditures online,
+                spatially distributed by country and prepared them visually appealing.
+                The focus is on the use of the initial information for people who need start-up assistance in
+                e-commerce.
             </p>
         </div>
-        
+
         <div class="entry__data">
             <div class="entry__data__item">
                 <div>
                     <img class="entry__data__item__img" src="/img/icons/coins_orange.png">
                 </div>
 
+                <p class="entry__data__item__info">Worldwide E-Commerce Sales 2016</p>
                 <span class="entry__data__item__head">{{$type}} {{$aggregatedSales}}</span>
-                <p class="entry__data__item__info">E-Commerce Sales 2016</p>
             </div>
 
             <div class="entry__data__item">
@@ -38,8 +41,8 @@
                     <img class="entry__data__item__img" src="/img/icons/basket_orange.png">
                 </div>
 
+                <p class="entry__data__item__info">Future Worldwide E-Commerce Sales 2021</p>
                 <span class="entry__data__item__head">{{$type}} {{$aggregatedFutureSales}}</span>
-                <p class="entry__data__item__info">Future E-Commerce Sales 2021</p>
             </div>
 
             <div class="entry__data__item">
@@ -48,18 +51,20 @@
                 </div>
 
                 <span class="entry__data__item__head">{{$salesOfCountry->name}}</span>
-                <p class="entry__data__item__info">
-                    @if (strcmp($salesOfCountry->sales, "") === 0)
-                        No current sales available
-                    @else
-                        Current Sale: {{$type}} {{$salesOfCountry->sales}}
-                    @endif
-                </p>
+                @if (strcmp($salesOfCountry->sales, "") === 0)
+                    <p class="entry__data__item__info">
+                        No Sales of 2016 Available
+                    </p>
+                @else
+                    <p class="entry__data__item__info">Sale 2016</p>
+                    <p class="entry__data__item__head">{{$type}} {{$salesOfCountry->sales}}</p>
+                @endif
+
             </div>
         </div>
 
         <div class="entry__more">
-            <p>Curious about our Google-Shopping Countries?</p>
+            <p>Curious About our Google Shopping Countries?</p>
             <span><a href="/overview" class="entry__more__button">Learn more</a> </span>
         </div>
 
