cc8a9769cb450fa1a06ef9ba003391bf80b7511a
manuela_posch <manuela.posch95@gmail.com>
2017-06-27 10:47:46 +0200

    HEADER CSS

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,140 +1,147 @@
-<!-- <div class="header">
-    <img class="header__img" src="https://whoopapp.com/images/logos/whoop-logo-72.png?r=2.01">
-    <a href="/overview" class="header__nav">OVERVIEW</a>
-    <a href="https://whoopapp.com/en/features/" class="header__nav">FEATURES</a>
-    <a href="https://whoopapp.com/en/pricing/" class="header__nav">PRICING</a>
-    <a href="https://whoopapp.com/blog/" class="header__nav">BLOG</a>
-    <a href="https://whoopapp.com/en/events/" class="header__nav">EVENTS</a>
-    <a href="https://whoopapp.com/en/events/" class="header__nav">RESSOURCES</a>
-    <ul class="header__navbar">
-    <a href="https://whoopapp.com/en/signup/" class="header__nav">Signup</a>
-    <a href="http://start.whoopapp.com/" class="header__nav" target="_blank">Login</a>
-    <span class="header__nav" class="dropdown">
-        ENGLISH
-    <ul class="dropdown-content">
-        <li >
-            <a href="/en">ENGLISH</a>
-        </li>
+<!--<div class="header">
+   <img class="header__img" src="https://whoopapp.com/images/logos/whoop-logo-72.png?r=2.01">
+   <a href="/overview" class="header__nav">OVERVIEW</a>
+   <a href="https://whoopapp.com/en/features/" class="header__nav">FEATURES</a>
+   <a href="https://whoopapp.com/en/pricing/" class="header__nav">PRICING</a>
+   <a href="https://whoopapp.com/blog/" class="header__nav">BLOG</a>
+   <a href="https://whoopapp.com/en/events/" class="header__nav">EVENTS</a>
+   <a href="https://whoopapp.com/en/events/" class="header__nav">RESSOURCES</a>
+   <ul class="header__navbar">
+   <a href="https://whoopapp.com/en/signup/" class="header__nav">Signup</a>
+   <a href="http://start.whoopapp.com/" class="header__nav" target="_blank">Login</a>
+   <span class="header__nav" class="dropdown">
+       ENGLISH
+   <ul class="dropdown-content">
+       <li >
+           <a href="/en">ENGLISH</a>
+       </li>
 
-        <li >
-            <a href="/de">DEUTSCH</a>
-        </li>
+       <li >
+           <a href="/de">DEUTSCH</a>
+       </li>
 
-        <li class="lang-switcher-fr">
-            <a class="lang-switch-fr-" href="/fr">FRANÇAIS</a>
-        </li>
+       <li class="lang-switcher-fr">
+           <a class="lang-switch-fr-" href="/fr">FRANÇAIS</a>
+       </li>
 
-        <li class="lang-switcher-es">
-            <a class="lang-switch-es-" href="/es">ESPAÑOL</a>
-        </li>
+       <li class="lang-switcher-es">
+           <a class="lang-switch-es-" href="/es">ESPAÑOL</a>
+       </li>
 
-        <li class="lang-switcher-it">
-            <a class="lang-switch-it-" href="/it">ITALIANO</a>
-        </li>
-        <li class="lang-switcher-tr">
-            <a class="lang-switch-tr-" href="/tr">TÜRKÇE</a>
-        </li>
-        <li class="lang-switcher-pl">
-            <a class="lang-switch-pl-" href="/pl">POLSKI</a>
-        </li>
-    </span>
-    </ul>
-    </ul>
-    <hr/>
+       <li class="lang-switcher-it">
+           <a class="lang-switch-it-" href="/it">ITALIANO</a>
+       </li>
+       <li class="lang-switcher-tr">
+           <a class="lang-switch-tr-" href="/tr">TÜRKÇE</a>
+       </li>
+       <li class="lang-switcher-pl">
+           <a class="lang-switch-pl-" href="/pl">POLSKI</a>
+       </li>
+   </span>
+   </ul>
+   </ul>
+   <hr/>
 </div>
 
+
 <div id="main-navbar">
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
+   <nav class="navbar navbar-default">
+       <div class="container-fluid">
 
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a id="logo" title="Start..." class="navbar-brand" href="/en/">
-                    <img src="https://whoopapp.com/images/logos/whoop-logo-72.png" srcset="https://whoopapp.com/images/logos/whoop-logo-72.png 1x, https://whoopapp.com/images/logos/whoop-logo-144.png 2x" alt="Whoop! Logo">
-                </a>
-            </div>
+           <div class="navbar-header">
+               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                   <span class="sr-only">Toggle navigation</span>
+                   <span class="icon-bar"></span>
+                   <span class="icon-bar"></span>
+                   <span class="icon-bar"></span>
+               </button>
+               <a id="logo" title="Start..." class="navbar-brand" href="/en/">
+                   <img src="https://whoopapp.com/images/logos/whoop-logo-72.png" srcset="https://whoopapp.com/images/logos/whoop-logo-72.png 1x, https://whoopapp.com/images/logos/whoop-logo-144.png 2x" alt="Whoop! Logo">
+               </a>
+           </div>
 
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="">
-                        <a href="/en/features/">Features</a>
-                    </li>
-                    <li class="">
-                        <a href="/en/pricing/">Pricing</a>
-                    </li>
-                    <li class="">
-                        <a href="http://blog.whoopapp.com/">Blog</a>
-                    </li>
-                    <li class="dropdown leandropdown">
-                        <span class="current-lang-btn" id="rLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          RESOURCES
-                          <i class="fa fa-angle-down" aria-hidden="true"></i>
-                        </span>
-                        <ul class="dropdown-menu" aria-labelledby="dLabel">
-                            <li>
-                                <a href="/en/faq/">FAQ</a>
-                            </li>
-                            <li>
-                                <a href="/google-shopping-compendium/">Compendium</a>
-                            </li>
-                            <li>
-                                <a href="/en/resources/">Documents</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+               <ul class="nav navbar-nav">
+                   <li class="">
+                       <a href="/en/features/">FEATURES</a>
+                   </li>
+                   <li class="">
+                       <a href="/en/pricing/">PRICING</a>
+                   </li>
+                   <li class="">
+                       <a href="http://blog.whoopapp.com/">BLOG</a>
+                   </li>
+                   <li class="">
+                       <a href="http://blog.whoopapp.com/">EVENTS</a>
+                   </li>
+                   <li class="dropdown leandropdown">
+                       <span class="current-lang-btn" id="rLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                         RESOURCES
+                         <i class="fa fa-angle-down" aria-hidden="true"></i>
+                       </span>
+                       <ul class="dropdown-menu" aria-labelledby="dLabel">
+                           <li>
+                               <a href="/en/faq/">FAQ</a>
+                           </li>
+                           <li>
+                               <a href="/google-shopping-compendium/">COMPENDIUM</a>
+                           </li>
+                           <li>
+                               <a href="/en/resources/">DOCUMENTS</a>
+                           </li>
+                           <li>
+                               <a href="/en/resources/">TOOLS</a>
+                           </li>
+                       </ul>
+                   </li>
+               </ul>
 
 
-                <ul class="nav navbar-nav navbar-right">
+               <ul class="nav navbar-nav navbar-right">
 
-                    <li class="nav-signup-cta ">
-                        <a href="/en/signup/" data-event-category="Referral to Signup" data-event-action="click" data-event-label="Signup Menu button / Header">Signup</a>
-                    </li>
-                    <li class="">
-                        <a href="http://start.whoopapp.com/" target="_blank">Login</a>
-                    </li>
+                   <li class="nav-signup-cta ">
+                       <a href="/en/signup/" data-event-category="Referral to Signup" data-event-action="click" data-event-label="Signup Menu button / Header">SIGNUP</a>
+                   </li>
+                   <li class="">
+                       <a href="http://start.whoopapp.com/" target="_blank">LOGIN</a>
+                   </li>
 
-                    <li class="dropdown language-switch">
-                        <span class="current-lang-btn" id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          ENGLISH
-                          <i class="fa fa-angle-down" aria-hidden="true"></i>
-                        </span>
-                        <ul class="dropdown-menu" aria-labelledby="dLabel">
-                            <li class="lang-switcher-en preselected-lang">
-                                <a class="lang-switch-en-" href="/en">ENGLISH</a>
-                            </li>
+                   <li class="dropdown language-switch">
+                       <span class="current-lang-btn" id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                         ENGLISH
+                         <i class="fa fa-angle-down" aria-hidden="true"></i>
+                       </span>
+                       <ul class="dropdown-menu" aria-labelledby="dLabel">
+                           <li class="lang-switcher-en preselected-lang">
+                               <a class="lang-switch-en-" href="/en">ENGLISH</a>
+                           </li>
 
-                            <li class="lang-switcher-de">
-                                <a class="lang-switch-de-" href="/de">DEUTSCH</a>
-                            </li>
+                           <li class="lang-switcher-de">
+                               <a class="lang-switch-de-" href="/de">DEUTSCH</a>
+                           </li>
 
-                            <li class="lang-switcher-fr">
-                                <a class="lang-switch-fr-" href="/fr">FRANÇAIS</a>
-                            </li>
+                           <li class="lang-switcher-fr">
+                               <a class="lang-switch-fr-" href="/fr">FRANÇAIS</a>
+                           </li>
 
-                            <li class="lang-switcher-es">
-                                <a class="lang-switch-es-" href="/es">ESPAÑOL</a>
-                            </li>
+                           <li class="lang-switcher-es">
+                               <a class="lang-switch-es-" href="/es">ESPAÑOL</a>
+                           </li>
 
-                            <li class="lang-switcher-it">
-                                <a class="lang-switch-it-" href="/it">ITALIANO</a>
-                            </li>
-                            <li class="lang-switcher-tr">
-                                <a class="lang-switch-tr-" href="/tr">TÜRKÇE</a>
-                            </li>
-                            <li class="lang-switcher-pl">
-                                <a class="lang-switch-pl-" href="/pl">POLSKI</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+                           <li class="lang-switcher-it">
+                               <a class="lang-switch-it-" href="/it">ITALIANO</a>
+                           </li>
+                           <li class="lang-switcher-tr">
+                               <a class="lang-switch-tr-" href="/tr">TÜRKÇE</a>
+                           </li>
+                           <li class="lang-switcher-pl">
+                               <a class="lang-switch-pl-" href="/pl">POLSKI</a>
+                           </li>
+                       </ul>
+                   </li>
+               </ul>
+           </div>
+       </div>
+   </nav>
 </div>
 -->
